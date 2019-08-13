c8c36c9b63b6ed38b86b65e3ba87790693710594
axelgar <axel.garcia94@gmail.com>
2019-08-13 08:11:45 +0200

    finish

--- models/user.js
+++ models/user.js
@@ -2,8 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: String,
-  password: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
 }, {
   timestamps: {
     createdAt: 'created_at',
